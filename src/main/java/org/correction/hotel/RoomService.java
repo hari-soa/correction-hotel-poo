6bee7276ac64b04b2194e885b79a55055c62bb8e
hari-soa <hei.zarasoa@gmail.com>
2026-07-14 22:32:55 +0300

    final version : roomservice.java

--- src/main/java/org/correction/hotel/RoomService.java
+++ src/main/java/org/correction/hotel/RoomService.java
@@ -1,5 +1,43 @@
 package org.correction.hotel;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomService {
 
+    private List<Room> allRooms;
+    private List<RoomReservation> allReservations;
+
+    public List<Room> searchFreeRoom(BigDecimal minPrice, BigDecimal maxPrice, Instant startDate, Instant endDate) {
+        List<Room> freeRooms = new ArrayList<>();
+
+        for (Room room : allRooms) {
+
+            BigDecimal roomPrice = BigDecimal.valueOf(room.getPrice());
+
+            boolean priceOk = roomPrice.compareTo(minPrice) >= 0 && roomPrice.compareTo(maxPrice) <= 0;
+
+            if (priceOk) {
+                if (isRoomFree(room, startDate, endDate)) {
+                    freeRooms.add(room);
+                }
+            }
+        }
+
+        return freeRooms;
+    }
+
+    private boolean isRoomFree(Room room, Instant startDate, Instant endDate) {
+        for (RoomReservation res : allReservations) {
+            if (res.getRoom().getId().equals(room.getId())) {
+
+                if (res.getStartDate().isBefore(endDate) && res.getEndDate().isAfter(startDate)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }

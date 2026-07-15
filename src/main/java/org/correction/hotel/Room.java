b41f816dab30eba74e9bb54d1fa602874085fc8a
hari-soa <hei.zarasoa@gmail.com>
2026-07-15 12:55:25 +0300

    room update

--- src/main/java/org/correction/hotel/Room.java
+++ src/main/java/org/correction/hotel/Room.java
@@ -4,19 +4,11 @@ import lombok.Getter;
 
 import java.util.List;
 
-import jakarta.persistence.*;
-
 @Getter
 
-@Entity
-@Table(name = "room")
 public class Room {
-    @Id
-    @Column(name = "id")
     private  String id;
-    @Column(name = "price", nullable = false)
     private double price;
-    @Column(name = "capacity", nullable = false)
     private int capacity;
     private List<Feature> features;
 }
